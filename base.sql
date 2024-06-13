c969852d9194b995078d608a97a99d300dae8b3f
Luthfi <luthfi552@gmail.com>
2024-06-13 07:31:11 +0700

    init

---
+++ base.sql
@@ -0,0 +1,10 @@
+-- Active: 1713325435843@@127.0.0.1@5432@postgres
+CREATE TABLE "users"(
+    "id" SERIAL PRIMARY KEY,
+    "full_name" VARCHAR(80),
+    "email" VARCHAR(80),
+    "password" VARCHAR(100),
+    "created_at" TIMESTAMP DEFAULT NOW(),
+    "updated_at" TIMESTAMP
+);
+-- INSERT INTO "users"("full_name", "email", "password") VALUES('John Doe', 'johndoe@mail.com', 'password');
